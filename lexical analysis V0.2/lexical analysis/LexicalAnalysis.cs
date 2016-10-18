e868b824abfce8004c5773d33ccf56d7a75f3244
amin jamal <aminjamal10@gmail.com>
2016-10-18 23:35:27 +0330

    V0.2 Some bugs were fixed

---
+++ lexical analysis V0.2/lexical analysis/LexicalAnalysis.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lexical_analysis
+{
+    class LexicalAnalysis
+    {
+
+       
+        public string TokenTokenizers(string Input)
+        {
+
+
+            return "";
+        }
+
+    }
+}
